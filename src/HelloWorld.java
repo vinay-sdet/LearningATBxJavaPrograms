b821fd970106c07693fca1a7607d26c91a11e50d
vinay-sdet <vgarge@gmail.com>
2025-01-10 07:53:41 +0530

    Added a comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
 public class HelloWorld {
-}
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+       /*  This is an example of multiline comment_
+        Let us be sure that it is commented otherwise it will show us the error.
+
+       */
+    }
+    }
+
